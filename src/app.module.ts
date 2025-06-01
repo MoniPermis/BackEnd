ef59705b69f72f8fca02746b39ca56c802c5ed6a
HugoN1 <hugo.noury@epitech.eu>
2025-06-01 12:29:26 +0200

    feat: add AvailabilityScheduleService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AvailabilityScheduleService } from './availability_schedule/availability_schedule.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AvailabilityScheduleService],
 })
 export class AppModule {}
