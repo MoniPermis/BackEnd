a4b0489feea1fb50c2b922b10b8f3d3771c3a654
HugoN1 <hugo.noury@epitech.eu>
2025-06-03 18:54:45 +0200

    refactor: remove unused instructor service

--- src/instructors/instructors.module.ts
+++ src/instructors/instructors.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { InstructorsService } from './instructors.service';
 import { InstructorsController } from './instructors.controller';
 import { AvailabilityScheduleModule } from '../availability_schedule/availability_schedule.module';
 
 @Module({
   imports: [AvailabilityScheduleModule],
   controllers: [InstructorsController],
-  providers: [InstructorsService],
+  providers: [],
 })
 export class InstructorsModule {}
