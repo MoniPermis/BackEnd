193778f64422a4b6a64b83ff7680c69f7e37a945
HugoN1 <hugo.noury@epitech.eu>
2025-06-04 18:49:00 +0200

    feat: add a route to retrieve specific instructor's all availabilities

--- src/instructors/instructors.controller.ts
+++ src/instructors/instructors.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Body, Param, ParseIntPipe, Get } from '@nestjs/common';
 import { AvailabilityScheduleService } from '../availability_schedule/availability_schedule.service';
 import { CreateAvailabilityScheduleDto } from '../availability_schedule/dto';
 
@@ -9,5 +9,10 @@ export class InstructorsController {
   @Post(':instructorId/availability')
   async createAvailability(@Param('instructorId', ParseIntPipe) instructorId: number, @Body() availabilityDto: CreateAvailabilityScheduleDto) {
     return this.availabilityScheduleService.createAvailability(instructorId, availabilityDto);
+  }
+
+  @Get(':instructorId/availability')
+  async getAvailabilities(@Param('instructorId', ParseIntPipe) instructorId: number) {
+    return this.availabilityScheduleService.getAllAvailabilitiesByInstructorId(instructorId);
   }
 }
