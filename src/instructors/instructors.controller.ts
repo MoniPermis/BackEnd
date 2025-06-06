8b548f92bd35791e486d96bb1b65324445a31b33
HugoN1 <hugo.noury@epitech.eu>
2025-06-06 19:54:47 +0200

    feat: add DELETE /instructors/instructorId/availability/availabilityId route.

--- src/instructors/instructors.controller.ts
+++ src/instructors/instructors.controller.ts
@@ -4,7 +4,7 @@ import {
   Body,
   Param,
   ParseIntPipe,
-  Get, Put,
+  Get, Put, Delete, HttpCode,
 } from '@nestjs/common';
 import { AvailabilityScheduleService } from '../availability_schedule/availability_schedule.service';
 import { CreateAvailabilityScheduleDto } from '../availability_schedule/dto';
@@ -45,6 +45,18 @@ export class InstructorsController {
       instructorId,
       availabilityId,
       availabilityDto,
+    );
+  }
+
+  @HttpCode(204)
+  @Delete(':instructorId/availability/:availabilityId')
+  async deleteAvailability(
+    @Param('instructorId', ParseIntPipe) instructorId: number,
+    @Param('availabilityId', ParseIntPipe) availabilityId: number,
+  ) {
+    return this.availabilityScheduleService.deleteAvailability(
+      instructorId,
+      availabilityId,
     );
   }
 }
