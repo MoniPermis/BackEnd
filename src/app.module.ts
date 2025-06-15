36f54a01e89d40ae3af25152d403b0370fa9d040
HugoN1 <hugo.noury@epitech.eu>
2025-06-15 23:11:35 +0200

    refactor: update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InstructorsModule } from './instructors/instructors.module';
 import { ScheduleValidationModule } from './schedule_validation/schedule_validation.module';
+import { AppointmentService } from './appointment/appointment.service';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
-  imports: [InstructorsModule, ScheduleValidationModule],
+  imports: [InstructorsModule, ScheduleValidationModule, AppointmentModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppointmentService],
 })
 export class AppModule {}
