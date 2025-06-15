bc94c8969b48b891b1a80fb254cdcec2e153ad5c
HugoN1 <hugo.noury@epitech.eu>
2025-06-15 23:09:04 +0200

    refactor: remove migrations because of persistent sequences errors

--- prisma/migrations/20250602165003_modify_availability_schedule_system/migration.sql
+++
@@ -1,9 +0,0 @@
-
--- AlterTable
-ALTER TABLE "availability_schedule" DROP COLUMN "day_of_week",
-DROP COLUMN "effective_date",
-DROP COLUMN "end_time",
-DROP COLUMN "start_time",
-ADD COLUMN     "end_datetime" DATE NOT NULL,
-ADD COLUMN     "recurrence_rule" VARCHAR(255),
-ADD COLUMN     "start_datetime" DATE NOT NULL
