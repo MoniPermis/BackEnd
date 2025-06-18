bb636b3deb9b46e3eb1dbfdc097793f8cab16a9b
HugoN1 <hugo.noury@epitech.eu>
2025-06-18 20:40:20 +0200

    feat: add error handling and appointment conflicts checking

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -1,12 +1,50 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateAppointmentDto } from './dto';
+import { ScheduleValidationService } from '../schedule_validation/schedule_validation.service';
 
 @Injectable()
 export class AppointmentService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private scheduleValidation: ScheduleValidationService,
+  ) {}
 
   async createAppointment(appointmentData: CreateAppointmentDto) {
+    const instructor = await this.prismaService.instructor.findUnique({
+      where: { id: appointmentData.instructorId },
+    });
+    if (!instructor) {
+      throw new Error(
+        `Moniteur avec l'ID ${appointmentData.instructorId} non trouvé`,
+      );
+    }
+
+    const student = await this.prismaService.student.findUnique({
+      where: { id: appointmentData.studentId },
+    });
+    if (!student) {
+      throw new Error(
+        `Étudiant avec l'ID ${appointmentData.studentId} non trouvé`,
+      );
+    }
+
+    const meetingPoint = await this.prismaService.meetingPoint.findUnique({
+      where: { id: appointmentData.meetingPointId },
+    });
+    if (!meetingPoint) {
+      throw new Error(
+        `Point de rencontre avec l'ID ${appointmentData.meetingPointId} non trouvé`,
+      );
+    }
+
+    await this.scheduleValidation.checkAppointmentConflicts(
+      appointmentData.instructorId,
+      new Date(appointmentData.startTime),
+      new Date(appointmentData.endTime),
+      undefined,
+    );
+
     return this.prismaService.appointment.create({
       data: {
         studentId: appointmentData.studentId,
@@ -17,7 +55,7 @@ export class AppointmentService {
         description: appointmentData.description || null,
         createdAt: new Date(),
         modifiedAt: new Date(),
-      }
+      },
     });
   }
 }
