2774de2a90dbdee87d14215f0a9bda54c55b83f0
HugoN1 <hugo.noury@epitech.eu>
2025-06-18 21:14:07 +0200

    refactor: delete unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InstructorsModule } from './instructors/instructors.module';
 import { ScheduleValidationModule } from './schedule_validation/schedule_validation.module';
-import { AppointmentService } from './appointment/appointment.service';
 import { AppointmentModule } from './appointment/appointment.module';
 import { MeetingPointsModule } from './meeting_points/meeting_points.module';
 
