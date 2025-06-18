621566fcfd844a9d6a334a43915eee95f2010768
HugoN1 <hugo.noury@epitech.eu>
2025-06-18 20:41:32 +0200

    feat: add appointment module

---
+++ src/appointment/appointment.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppointmentController } from './appointment.controller';
+import { AppointmentService } from './appointment.service';
+import { ScheduleValidationModule } from '../schedule_validation/schedule_validation.module';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [ScheduleValidationModule, PrismaModule],
+  controllers: [AppointmentController],
+  providers: [AppointmentService],
+})
+export class AppointmentModule {}
