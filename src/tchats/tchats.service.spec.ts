c97b38ceba08a2945cefa20f6bce9272d140a65e
virgilecrtl <virgile.courtial@epitech.eu>
2025-06-20 17:59:36 +0200

    fix:import error

--- src/tchats/tchats.service.spec.ts
+++ src/tchats/tchats.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TchatsService } from './tchats.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('TchatsService', () => {
   let service: TchatsService;
