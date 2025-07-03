2713a762a36e6d425e9e140674585b2bae1f2242
HugoN1 <hugo.noury@epitech.eu>
2025-07-03 19:41:04 +0200

    refactor: add Instructors module import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy';
+import { InstructorsModule } from '../instructors/instructors.module';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), InstructorsModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
